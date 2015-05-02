db796f329c6b129e6f0ef1b5ae4f6e2df1ff8784
meditate <aroundofyou@gmail.com>
2015-05-02 16:32:45 +0200

    -edditting admins controller

--- app/controllers/admin/attendences_controller.rb
+++ app/controllers/admin/attendences_controller.rb
@@ -1,3 +1,33 @@
 class Admin::AttendencesController < Admin::BaseController
+  before_action :load_event
 
+  def index
+    @attendences=Attendence.all
+  end
+
+  def new
+    @attendence=Attendence.new
+  end
+
+  def create
+    @attendence=Attendence.new
+    @attendence.user_id=current_user.id
+    @attendence.event_id=@event.id
+
+    if @attendence.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def attendence_params
+    params.require(:attendence).permit()
+  end
+
+  def load_event
+    @event = Event.find(params[:event_id])
+  end
 end
