1b4cf1bac14a68f3f753b9d54f2e025752e0e51c
Khotinets <l.khotinets@gmail.com>
2015-05-05 19:42:22 +0300

    Admin can create and manage annoucements. User can list all announcements or choose option show by clicking title of announcemet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :admins
   devise_for :users
   root 'home#index'
-
+resources :announcements
   namespace :admin do
+    resources :announcements
     root 'home#index'
   end
 end
